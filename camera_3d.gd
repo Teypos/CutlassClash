1685cf34064be471c9b4cafcf811efd15445207b
Teypos <164430372+Teypos@users.noreply.github.com>
2025-04-22 13:13:19 +0100

    many

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,17 +1,17 @@
 extends Node3D
 #var sens=2
 signal set_cam_rotation(_cam_rotation : float)
-@onready var yaw_camera3d = $Yaw
-@onready var pitch_camera3d = $Yaw/Pitch
+@onready var cam_target_camera3d = $cam_target
+@onready var pitch_camera3d = $cam_target/Pitch
 @onready var camera = $Pitch/Camera3D
 
-var yaw : float=0
+var cam_target : float=0
 var pitch : float=0
 
-var yaw_sens : float=0.1
+var cam_target_sens : float=0.1
 var pitch_sens : float=0.1
 
-var yaw_accel : float=1
+var cam_target_accel : float=1
 var pitch_accel : float=1
 
 # Called when the node enters the scene tree for the first time.
@@ -22,19 +22,19 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		yaw += -event.relative.x * yaw_sens
+		cam_target += -event.relative.x * cam_target_sens
 		pitch += -event.relative.y * pitch_sens
 		
 		
 
 func _physics_process(delta: float) -> void:
-	#yaw_camera3d.rotation_degrees.y = lerp(yaw_camera3d.rotation_degrees.y, yaw, yaw_accel * delta)
+	#cam_target_camera3d.rotation_degrees.y = lerp(cam_target_camera3d.rotation_degrees.y, cam_target, cam_target_accel * delta)
 	#pitch_camera3d.rotation_degrees.x = lerp(pitch_camera3d.rotation_degrees.x, pitch, pitch_accel * delta)
 	
-	yaw_camera3d.rotation_degrees.y = yaw
+	cam_target_camera3d.rotation_degrees.y = cam_target
 	pitch_camera3d.rotation_degrees.x = pitch
 	
-	set_cam_rotation.emit(yaw_camera3d.rotation.y)
+	set_cam_rotation.emit(cam_target_camera3d.rotation.y)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
